61203bff6a06239a95612568966fbbb5d4c6fbd3
vminojosa <vinojosa@vassar.edu>
2025-09-23 10:19:54 -0400

    console.log for startDate, endDate when testing workflow

--- scripts/graph-downloads.js
+++ scripts/graph-downloads.js
@@ -19,6 +19,7 @@ async function getDownloadCount(pkg) {
   startDate.setFullYear(startDate.getFullYear() - 10);
   startDate.toISOString().split('T')[0];
   endDate.toISOString().split('T')[0];
+  console.log(startDate, endDate);
 
   const url = `https://npm-stat.com/api/download-counts?package=${pkg}&from=${startDate}&until=${endDate}/`;
   const res = await fetch(url);
